aca454dea31bf97d7ec3b2cec3f0c3b84ee3dfc2
Arthur Picerna <arthurpicerna@gmail.com>
2018-04-09 17:25:36 -0500

    Source update: test

--- deploy.sh
+++ deploy.sh
@@ -9,32 +9,26 @@ fi
 
 echo -e "\033[0;32mSyncing source files with repo...\033[0m"
 
+git checkout source
 git rm -r public
 git add .
 
 git commit -m "Source update: $msg"
-
 git push origin source
 
 echo -e "\033[0;32mDeploying updates to GitHub...\033[0m"
+
+git clone "https://github.com/decksterr/decksterr.github.io.git"
+git mv decksterr.gihub.io public
 
 # Build the project.
 #hugo # if using a theme, replace with `hugo -t <YOURTHEME>`
 hugo -t hyde-hyde
 
-# Go To Public folder
 cd public
 
-git remote add origin "https://github.com/decksterr/decksterr.github.io.git"
-
-# Add changes to git.
 git add .
-
-# Commit changes.
-
 git commit -m "Build update: $msg"
-
-# Push source and build repos.
 git push origin master
 
 # Come Back up to the Project Root
