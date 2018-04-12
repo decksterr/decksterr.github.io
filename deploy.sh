1e056a0f8939593c25eeb1423a9c37b3af0f2021
Arthur Picerna <arthurpicerna@gmail.com>
2018-04-12 15:13:22 -0500

    Source update: new deploy script

--- deploy.sh
+++ deploy.sh
@@ -20,7 +20,7 @@ echo -e "\n---\n---\e[38;5;36m Syncing with master branch... \e[0m\n---\n"
 
 git clone "git@github.com:decksterr/decksterr.github.io.git"
 
-mv decksterr.github.io.git public
+mv decksterr.github.io public
 cd public
 
 git checkout master 
